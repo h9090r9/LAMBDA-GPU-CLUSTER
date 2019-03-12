74491ab4b20d35bf09f9fedb9572b740f5a7fb1e
Mumtaz Hussain Soomro <47660542+HUSSAIN2388@users.noreply.github.com>
2019-03-12 12:13:42 +0100

    Create add_user_lambda.sh

---
+++ Add_user_accounts/add_user_lambda.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+#./add_user_lambda.sh UserName sudo/None
+
+# Force running as sudo
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root"
+   exit 1
+fi
+
+
+# Add user based on first argument, check if sudo on second
+if [ "$2" = "sudo" ]; then
+  adduser "$1" --ingroup researchers
+  adduser "$1" --ingroup sudo
+else
+  adduser "$1" --ingroup masterStudents
+fi
+
+# Force user to change password on next log-in
+passwd --expire "$1"
+
+# Create a folder for them on the data disk
+mkdir -p "/media/data/${1}"
+chown -R ${1}:${1} "/media/data/${1}"
+chmod -R 755 "/media/data/${1}"
+
+
+# Also add to slurm system
+sacctmgr -i create user $1 defaultaccount=researchers partition=GPU
+
+edquota "$1"
